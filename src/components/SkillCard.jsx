5031c82beeb7210b0ee1ff95feef4534c42f9ba2
Marco Schiavone <95691374+Marco-Skiavone@users.noreply.github.com>
2025-03-18 11:50:34 +0100

    Update SkillCard.jsx

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -6,7 +6,7 @@ const SkillCard = ({ image, description, size , radius , sizeLock}) => {
         <OverlayTrigger placement="auto" delay={{ show: 100, hide: 200 }} overlay={<Tooltip>{description}</Tooltip>}>
             <Card className="d-flex justify-content-center align-content-center shadow-sm ratio ratio-1x1 border-0 skill-card"
                   style={{ width: (sizeLock) ? '4rem' : size , height: (sizeLock) ? '4rem' : size }}>
-                <Card.Img src={image} className={(radius ? "p-0" : "p-1")} style={{ borderRadius : (radius ? "inherit" : "0") }}></Card.Img>
+                <Card.Img src={image} alt={description} className={(radius ? "p-0" : "p-1")} style={{ borderRadius : (radius ? "inherit" : "0") }}></Card.Img>
             </Card>
         </OverlayTrigger>
     );
