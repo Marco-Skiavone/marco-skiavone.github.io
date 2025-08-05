6df5a4442cf0209b43f99e5db931174f2f047692
Marco Schiavone <95691374+Marco-Skiavone@users.noreply.github.com>
2025-08-05 20:21:15 +0200

    minor changes to projects and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { BsTelegram, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
     return (
-        <footer id="contacts" className="bg-dark text-light text-center py-4">
+        <footer id="contacts" className="bg-dark text-light text-center py-5">
             <Container>
                 <span className="h2 text-center my-2">My Contacts</span>
                 <div className="my-3">
@@ -11,7 +11,7 @@ const Footer = () => {
                     <a target={'_blank'} href={"https://github.com/Marco-Skiavone"}><BsGithub size={45} className="mx-4" /></a>
                     <a target={'_blank'} href={"https://www.linkedin.com/in/marco-schiavone-587b3b203/"}><BsLinkedin size={45} className="mx-4" /></a>
                 </div>
-                <p className="m-0">
+                <p className="m-0 mt-4">
                     This website has been made by myself.
                 </p>
                 <p className="m-0 pb-2">
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -2,10 +2,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCard";
 
 const projects = [
-    { title: "FastFlow Schedule Management", description: "My Thesis Topic: Thread Scheduling (in Linux)", image: "/images/projects/fastflow.webp", link: "https://github.com/Marco-Skiavone/fastflow", when: 2025 },
+    { title: "FastFlow Schedule Management", description: "My Thesis Topic: Using Linux Scheduling (real-time) for FasFlow library (threads parallelism)", image: "/images/projects/fastflow.webp", link: "https://github.com/Marco-Skiavone/fastflow", when: 2025 },
     { title: "Software App Development", description: "Application Design & Iterative Development Strategies", image: "/images/projects/sas.webp", link: "https://github.com/Marco-Skiavone/Catering-implementation", when: 2025 },
     { title: "Web Development", description: "Use of routing technologies with: JS, Bootstrap, Express (node.js)...", image: "/images/projects/tweb.webp", link: "https://github.com/IvanLusso/LussoSchiavoneStefanettiIUM-Tweb", when: 2024 },
-    { title: "Thread Programming", description: "Threaded Mailing List Management (Java)", image: "/images/projects/threads.webp", link: "https://github.com/Marco-Skiavone/Programming-3-Project", when: 2024 },
+    { title: "Thread Programming", description: "Threaded Mailing List Management (in Java + JavaFX)", image: "/images/projects/threads.webp", link: "https://github.com/Marco-Skiavone/Programming-3-Project", when: 2024 },
     { title: "Data Analisys", description: "Overview of Pandas, Seaborn & other tools", image: "/images/projects/data_an.webp", link: "https://github.com/Marco-Skiavone/ium_dataset_processor", when: 2024 },
     { title: "Algorithm Studies", description: "Algorithms, Complexity & Data Structures Overview", image: "/images/projects/algo.png", link: "https://github.com/Marco-Skiavone/Algoritmi_e_Strutture_Dati", when: 2023 },
     // { title: "title", description: "desc", image: "https://via.placeholder.com/300", link: "#", when: 2024 }
